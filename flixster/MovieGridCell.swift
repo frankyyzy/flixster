d54dfa9200a0126e13b01dd7b6fa8bd21f8b4128
Ziyi Ye <ziy116@ucsd.edu>
2019-02-25 21:25:07 -0800

    basic features

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Ziyi Ye on 2/25/19.
+//  Copyright © 2019 Frank Ye. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var postView: UIImageView!
+}
